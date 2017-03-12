224b29ab51415dec9d90f693ba180ce5092f293e
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2017-03-12 09:55:50 -0500

    Refactoriza y crea metodo para eliminar un Empleado

--- src/main/java/pe/cibertec/tests/TestEmpleado.java
+++ src/main/java/pe/cibertec/tests/TestEmpleado.java
@@ -12,12 +12,11 @@ import pe.cibertec.modelo.Empleado;
 public class TestEmpleado {
 
 	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("Persistencia");
-	private static EntityManager manager;
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 
-		manager = factory.createEntityManager();
+		//EntityManager manager = factory.createEntityManager();
 
 		Empleado emp1 = new Empleado(1L, "Mejia Rojas", "Julio Edgar", new GregorianCalendar(1987, 10, 10).getTime());
 		Empleado emp2 = new Empleado(2L, "Mejias Rojas", "Ju Edgar", new GregorianCalendar(1987, 10, 10).getTime());
@@ -32,28 +31,47 @@ public class TestEmpleado {
 		updateEmpleado();
 
 		mostrarEmpleados();
+		
+		eliminarEmpleado();
+		
+		mostrarEmpleados();
 
+		//manager.close();
+	}
+
+	private static void eliminarEmpleado() {
+		//Eliminamos el empleado3
+		EntityManager manager = factory.createEntityManager();
+		manager.getTransaction().begin();
+		Empleado empleado = manager.find(Empleado.class,3L);
+		manager.remove(empleado);
+		manager.getTransaction().commit();
 		manager.close();
 	}
 
 	private static void updateEmpleado() {
+		EntityManager manager = factory.createEntityManager();
 		manager.getTransaction().begin();
 		Empleado encontrado = manager.find(Empleado.class, 3L);
 		encontrado.setNombres("Jose Luis");
 		encontrado.setApellidos("Mejilla Roja");
 		manager.getTransaction().commit();
+		manager.close();
 	}
 
 	private static void insertarEmpleado(Empleado emp) {
+		EntityManager manager = factory.createEntityManager();
 		manager.getTransaction().begin();
 		manager.persist(emp);
 		manager.getTransaction().commit();
-		//manager.close();
+		manager.close();
 	}
 
 	private static void mostrarEmpleados() {
+		EntityManager manager = factory.createEntityManager();
 		List<Empleado> empleados = manager.createQuery("from Empleado e").getResultList();
 		System.out.println("En esta Bd hay : " + empleados.size() + " empleados");
 		empleados.forEach((empleado) -> System.out.println(empleado));
+		manager.close();
 	}
 }
